ea50d202481d5bd6dcae7f614be7fda3d14dd7f9
Éverton M. Vieira <everton.muvi@gmail.com>
2022-05-04 18:14:31 -0300

    prototype development

--- test-dsk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-bash -v build.sh
-browserify build/index.js --debug -o public/index.js
-rm -rf ../qinpel-dsk/run/app/edmister
-mkdir ../qinpel-dsk/run/app/edmister
-cd public
-cp -r * ../../qinpel-dsk/run/app/edmister
-cd ..
